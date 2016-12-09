76102f7743db7e46ff0832e0202e7c8afe58a795
Dan Ravensloft <matthew.r.brades@googlemail.com>
2016-12-09 19:33:44 +0000

    As pointed out by Travis.

--- board.h
+++ board.h
@@ -111,7 +111,7 @@ static const uint64_t Rank7Mask = 0x00FF000000000000ULL;
 static const uint64_t Rank8Mask = 0xFF00000000000000ULL;
 
 // From Adam Hair's PSTs.
-extern int piecevals[7];
+extern int piecevals[7][2];
 extern int pst[6][2][64];
 
 extern int nodes;
